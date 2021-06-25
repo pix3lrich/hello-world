4508f34ef6a8388b1db87fd4331f0932df852009
pix3lrich <ricgeorge21@gmail.com>
2021-06-25 14:31:13 +0100

    Update updatefiles.sh

--- scripts/updatefiles.sh
+++ scripts/updatefiles.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "Starting file update..."
-echo $PWD
 cd ../sample-directory
 echo "Jenkins Testing Line" >> "file.conf"
+echo
+echo "New file contents:"
+echo "____________________________"
 cat  "file.conf"
+echo "____________________________"
+echo
 echo "File update complete."
